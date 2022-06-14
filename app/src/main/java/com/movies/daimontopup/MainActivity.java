dab89d245794a711716c5fd89f533825489c1da6
Shahain <shahinrhaman30@gmail.com>
2022-06-14 17:34:01 +0600

    add push notication

--- app/src/main/java/com/movies/daimontopup/MainActivity.java
+++ app/src/main/java/com/movies/daimontopup/MainActivity.java
@@ -18,6 +18,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         progressDialog=new ProgressDialog(this);
         packCalss=new PackCalss();
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
 
 
 
